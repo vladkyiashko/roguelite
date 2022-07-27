f2ea2ad116667ce926bb81a74a71515662be7866
vlad.kyiashko <vlad.kyiashko@gmail.com>
2022-07-27 08:32:30 +0300

    sprite flip move dir

--- Assets/Scripts/Common/AbstractMove.cs
+++ Assets/Scripts/Common/AbstractMove.cs
@@ -4,6 +4,7 @@ public abstract class AbstractMove : MonoBehaviour
 {
     [SerializeField] protected float Speed;
     [SerializeField] protected Rigidbody2D RB;
+    [SerializeField] protected SpriteRenderer Sprite;
     protected Vector3 MoveDir;
     
     protected virtual void FixedUpdate()
@@ -14,5 +15,19 @@ public abstract class AbstractMove : MonoBehaviour
         }
 
         RB.MovePosition(transform.position + MoveDir * Speed * Time.fixedDeltaTime);
+
+        SetSpriteDir();   
+    }
+
+    protected virtual void SetSpriteDir()
+    {
+        if (MoveDir.x > 0)
+        {
+            Sprite.flipX = false;
+        }
+        else if (MoveDir.x < 0)
+        {
+            Sprite.flipX = true;
+        }
     }
 }
